92df80a18c74db6b1d961d0c22f9108ca5b94299
Jannes.Jonkers <jjonkers2@gmail.com>
2016-04-18 16:34:07 +0200

    +Item pickup works now

--- src/itemFactory.js
+++ src/itemFactory.js
@@ -9,8 +9,8 @@ var itemSize = 40;
 var dropchance = 30;
 
 var itemCollisionHandler = function(item, player){
-    character.getCurrentUser().inventory.push(item.daddy);
-    item.destroy();
+    character.getCurrentUser().inventory.push(item.daddy.item);
+    item.daddy.entity.destroy();
 };
 
 itemFactory.dropRandomItem = function(level, x, y){
@@ -24,21 +24,21 @@ itemFactory.dropRandomItem = function(level, x, y){
 };
 
 itemFactory.createRandomItem = function(level){
-    var item;
+    var item = {};
     var randomNumber = Math.random();
 
     if(randomNumber >= 0 && randomNumber < 0.20 ){
         //Weapon
-        item = itemLibrary.random_weapon(level);
+        item.item = itemLibrary.random_weapon(level);
     } else if (randomNumber >= 0.20 && randomNumber < 0.45 ) {
         //Armour
-        item = itemLibrary.random_armour(level);
+        item.item = itemLibrary.random_armour(level);
     } else if (randomNumber >= 0.45 && randomNumber < 0.70 ){
         //Shield
-        item = itemLibrary.random_shield(level);
+        item.item = itemLibrary.random_shield(level);
     } else {
         //Hat
-        item = itemLibrary.random_hat(level);
+        item.item = itemLibrary.random_hat(level);
     }
 
     return item;
@@ -47,31 +47,31 @@ itemFactory.createRandomItem = function(level){
 itemFactory.drawItem = function(item, x, y){
     var sprite = game.add.graphics(0, 0);
 
-    if(item.type == "weapon"){
+    if(item.item.type == "weapon"){
         sprite.beginFill(0xffff00);
         shapes.item_weapon(sprite, itemSize);
-    }else if(item.type == "armour"){
+    }else if(item.item.type == "armour"){
         sprite.beginFill(0x00ff00);
         shapes.item_armour(sprite,itemSize);
-    }else if(item.type == "hat"){
+    }else if(item.item.type == "hat"){
         sprite.beginFill(0x0000ff);
         shapes.item_hat(sprite,itemSize);
-    }else if(item.type == "shield"){
+    }else if(item.item.type == "shield"){
         sprite.beginFill(0xffff00);
         shapes.item_shield(sprite,itemSize,0xff0000);
     }else {
         sprite.beginFill(0x222222);
     }
-    var item = game.add.sprite(x, y, sprite.generateTexture());
+    item.entity = game.add.sprite(x, y, sprite.generateTexture());
     sprite.destroy();
-    game.physics.p2.enable(item);
-    item.body.setCollisionGroup(game.itemCollisionGroup);
+    game.physics.p2.enable(item.entity);
+    item.entity.body.setCollisionGroup(game.itemCollisionGroup);
 
-    console.log(itemCollisionHandler);
-    item.body.collides(game.playerCollisionGroup, itemCollisionHandler);
-    item.body.collides(game.allCollisionGroups);
-    item.body.daddy = item;
-    item.body.debug = true;
+    //console.log(itemCollisionHandler);
+    item.entity.body.collides(game.playerCollisionGroup, itemCollisionHandler);
+    item.entity.body.collides(game.allCollisionGroups);
+    item.entity.body.daddy = item;
+    item.entity.body.debug = true;
 };
 
 module.exports = itemFactory;
